28fb670ac752c0b41b583349dbfe18b81672d07a
Jaspar Gupta <jaspar.gupta@gmail.com>
2023-04-07 19:23:10 +0100

    refactor: ts disallow negated nested where

--- src/types/where-nested.ts
+++ src/types/where-nested.ts
@@ -2,6 +2,7 @@ import Builder from '../builder';
 import Where from './base-where';
 
 export default interface WhereNested extends Where {
+  boolean: 'and' | 'or',
   type: 'Nested',
   value: Builder,
 }
